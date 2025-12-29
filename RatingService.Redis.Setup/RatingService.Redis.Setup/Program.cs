6856b61b78ec13f46e8e79cc38512a8d87142bd8
CodingFlow <3643313+CodingFlow@users.noreply.github.com>
2025-12-28 20:36:11 -0500

    Make Redis id field sortable to improve Aggregate queries.

--- RatingService.Redis.Setup/RatingService.Redis.Setup/Program.cs
+++ RatingService.Redis.Setup/RatingService.Redis.Setup/Program.cs
@@ -10,7 +10,7 @@ var muxer = ConnectionMultiplexer.Connect($"{host}:{port}");
 var db = muxer.GetDatabase();
 
 var schema = new Schema()
-            .AddTagField(new FieldName("$.Id", "id"))
+            .AddTagField(new FieldName("$.Id", "id"), sortable: true)
             .AddNumericField(new FieldName("$.UserId", "userId"))
             .AddNumericField(new FieldName("$.ServiceId", "serviceId"))
             .AddNumericField(new FieldName("$.Score", "score"));
