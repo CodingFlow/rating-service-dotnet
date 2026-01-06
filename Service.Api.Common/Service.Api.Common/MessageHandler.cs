2ff97c9f819ad6ee12582a0a87dde0302dea7c91
CodingFlow <3643313+CodingFlow@users.noreply.github.com>
2026-01-05 19:45:29 -0500

    Remove unused dependencies from MessageHandler.cs.

--- Service.Api.Common/Service.Api.Common/MessageHandler.cs
+++ Service.Api.Common/Service.Api.Common/MessageHandler.cs
@@ -1,12 +1,10 @@
 ﻿using System.Text.Json.Nodes;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Logging;
 using NATS.Client.JetStream;
 using NATS.Net;
 
 namespace Service.Api.Common;
 
-internal partial class MessageHandler(IMemoryCache memoryCache, IRequestDispatcher requestDispatcher, ILogger<MessageHandler> logger) : IMessageHandler
+internal partial class MessageHandler(IRequestDispatcher requestDispatcher) : IMessageHandler
 {
     public async ValueTask HandleMessage(NatsClient client, INatsJSMsg<Request<JsonNode>> message, CancellationToken cancellationToken)
     {
